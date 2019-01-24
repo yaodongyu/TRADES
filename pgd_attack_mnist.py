b2993008852442c4d6885a90030c8dda24227242
Yaodong Yu <yaodong.yu92@gmail.com>
2019-01-24 14:36:30 -0500

    demo code for pgd attack on MNIST

---
+++ pgd_attack_mnist.py
@@ -0,0 +1,167 @@
+from __future__ import print_function
+import os
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from torch.autograd import Variable
+import torch.optim as optim
+from torchvision import datasets, transforms
+from models.small_cnn import *
+from models.net_mnist import *
+
+
+parser = argparse.ArgumentParser(description='PyTorch MNIST PGD Attack Evaluation')
+parser.add_argument('--test-batch-size', type=int, default=200, metavar='N',
+                    help='input batch size for testing (default: 200)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--epsilon', default=0.3,
+                    help='perturbation')
+parser.add_argument('--num-steps', default=40,
+                    help='perturb number of steps')
+parser.add_argument('--step-size', default=0.01,
+                    help='perturb step size')
+parser.add_argument('--model-path',
+                    default='./checkpoints/model_mnist_smallcnn.pt',
+                    help='model for white-box attack evaluation')
+parser.add_argument('--source-model-path',
+                    default='./checkpoints/model_mnist_smallcnn.pt',
+                    help='source model for black-box attack evaluation')
+parser.add_argument('--target-model-path',
+                    default='./checkpoints/model_mnist_smallcnn.pt',
+                    help='target model for black-box attack evaluation')
+parser.add_argument('--white-box-attack', default=True,
+                    help='whether perform white-box attack')
+
+args = parser.parse_args()
+
+# settings
+use_cuda = not args.no_cuda and torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+
+# set up data loader
+transform_test = transforms.Compose([transforms.ToTensor(),])
+testset = torchvision.datasets.MNIST(root='../data', train=False, download=True, transform=transform_test)
+test_loader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size, shuffle=False, **kwargs)
+
+
+def _pgd_whitebox(model,
+                  X,
+                  y,
+                  epsilon=args.epsilon,
+                  num_steps=args.num_steps,
+                  step_size=args.step_size):
+    out = model(X)
+    err = (out.data.max(1)[1] != y.data).float().sum()
+    X_pgd = Variable(X.data, requires_grad=True)
+    for _ in range(num_steps):
+        opt = optim.SGD([X_pgd], lr=1e-3)
+        opt.zero_grad()
+
+        with torch.enable_grad():
+            loss = nn.CrossEntropyLoss()(model(X_pgd), y)
+        loss.backward()
+        eta = step_size * X_pgd.grad.data.sign()
+        X_pgd = Variable(X_pgd.data + eta, requires_grad=True)
+        eta = torch.clamp(X_pgd.data - X.data, -epsilon, epsilon)
+        X_pgd = Variable(X.data + eta, requires_grad=True)
+        X_pgd = Variable(torch.clamp(X_pgd, 0, 1.0), requires_grad=True)
+    err_pgd = (model(X_pgd).data.max(1)[1] != y.data).float().sum()
+    print('err pgd (white-box): ', err_pgd)
+    return err, err_pgd
+
+
+def _pgd_blackbox(model_target,
+                  model_source,
+                  X,
+                  y,
+                  epsilon=args.epsilon,
+                  num_steps=args.num_steps,
+                  step_size=args.step_size):
+    out = model_target(X)
+    err = (out.data.max(1)[1] != y.data).float().sum()
+    X_pgd = Variable(X.data, requires_grad=True)
+    for _ in range(num_steps):
+        opt = optim.SGD([X_pgd], lr=1e-3)
+        opt.zero_grad()
+
+        with torch.enable_grad():
+            loss = nn.CrossEntropyLoss()(model_source(X_pgd), y)
+        loss.backward()
+        eta = step_size * X_pgd.grad.data.sign()
+        X_pgd = Variable(X_pgd.data + eta, requires_grad=True)
+        eta = torch.clamp(X_pgd.data - X.data, -epsilon, epsilon)
+        X_pgd = Variable(X.data + eta, requires_grad=True)
+        X_pgd = Variable(torch.clamp(X_pgd, 0, 1.0), requires_grad=True)
+
+    err_pgd = (model_target(X_pgd).data.max(1)[1] != y.data).float().sum()
+    print('err pgd black-box: ', err_pgd)
+    return err, err_pgd
+
+
+def eval_adv_test_whitebox(model, device, test_loader):
+    """
+    evaluate model by white-box attack
+    """
+    model.eval()
+    robust_err_total = 0
+    natural_err_total = 0
+
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            # pgd attack
+            X, y = Variable(data, requires_grad=True), Variable(target)
+            err_natural, err_robust = _pgd_whitebox(model, X, y)
+            robust_err_total += err_robust
+            natural_err_total += err_natural
+    print('natural_err_total: ', natural_err_total)
+    print('robust_err_total: ', robust_err_total)
+
+
+def eval_adv_test_blackbox(model_target, model_source, device, test_loader):
+    """
+    evaluate model by black-box attack
+    """
+    model_target.eval()
+    model_source.eval()
+    robust_err_total = 0
+    natural_err_total = 0
+
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            # pgd attack
+            X, y = Variable(data, requires_grad=True), Variable(target)
+            err_natural, err_robust = _pgd_blackbox(model_target, model_source, X, y)
+            robust_err_total += err_robust
+            natural_err_total += err_natural
+    print('natural_err_total: ', natural_err_total)
+    print('robust_err_total: ', robust_err_total)
+
+
+def main():
+
+    if args.white_box_attack:
+        # white-box attack
+        print('pgd white-box attack')
+        model = SmallCNN().to(device)
+        model.load_state_dict(torch.load(args.model_path))
+
+        eval_adv_test_whitebox(model, device, test_loader)
+    else:
+        # black-box attack
+        print('pgd black-box attack')
+        model_target = SmallCNN().to(device)
+        model_target.load_state_dict(torch.load(args.target_model_path))
+        model_source = SmallCNN().to(device)
+        model_source.load_state_dict(torch.load(args.source_model_path))
+
+        eval_adv_test_blackbox(model_target, model_source, device, test_loader)
+
+
+if __name__ == '__main__':
+    main()

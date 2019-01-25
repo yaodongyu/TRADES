70051db4952785c96c4b7957647e6c76623fde02
Yaodong Yu <yaodong.yu92@gmail.com>
2019-01-25 18:21:37 -0500

    add l2 distance

--- trades.py
+++ trades.py
@@ -1,16 +1,27 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
 
 
-def squared_l2_norm(x):
-    flattened = x.view(x.shape[0], -1)
-    return (flattened ** 2).sum(1)
-
-
-def l2_norm(x):
-    return squared_l2_norm(x).sqrt()
+def perturb_kl(model,
+               x_natural,
+               step_size=0.003,
+               epsilon=0.031,
+               perturb_steps=10):
+    # define KL-loss
+    criterion_kl = nn.KLDivLoss(size_average=False)
+    # init adversarial example
+    x_adv = x_natural.detach() + 0.001 * torch.randn(x_natural.shape).cuda().detach()
+    for _ in range(perturb_steps):
+        x_adv.requires_grad_()
+        with torch.enable_grad():
+            loss_kl = criterion_kl(F.log_softmax(model(x_adv), dim=1),
+                                   F.softmax(model(x_natural), dim=1))
+        grad = torch.autograd.grad(loss_kl, [x_adv])[0]
+        x_adv = x_adv.detach() + step_size * torch.sign(grad.detach())
+        x_adv = torch.min(torch.max(x_adv, x_natural - epsilon), x_natural + epsilon)
+        x_adv = torch.clamp(x_adv, 0.0, 1.0)
+    return x_adv
 
 
 def trades_loss(model,
@@ -21,47 +32,22 @@ def trades_loss(model,
                 epsilon=0.031,
                 perturb_steps=10,
                 batch_size=128,
-                beta=1.0,
-                distance='l_inf'):
+                beta=1.0):
     # define KL-loss
     criterion_kl = nn.KLDivLoss(size_average=False)
 
     # generate adversarial example
     x_adv = x_natural.detach() + 0.001 * torch.randn(x_natural.shape).cuda().detach()
-    # L_infinity distance
-    if distance == 'l_inf':
-        for _ in range(perturb_steps):
-            x_adv.requires_grad_()
-            with torch.enable_grad():
-                loss_kl = criterion_kl(F.log_softmax(model(x_adv), dim=1),
-                                       F.softmax(model(x_natural), dim=1))
-            grad = torch.autograd.grad(loss_kl, [x_adv])[0]
-            x_adv = x_adv.detach() + step_size * torch.sign(grad.detach())
-            x_adv = torch.min(torch.max(x_adv, x_natural - epsilon), x_natural + epsilon)
-            x_adv = torch.clamp(x_adv, 0.0, 1.0)
-    # L_2 distance
-    elif distance == 'l_2':
-        for _ in range(perturb_steps):
-            x_adv.requires_grad_()
-            with torch.enable_grad():
-                loss_kl = criterion_kl(F.log_softmax(model(x_adv), dim=1),
-                                       F.softmax(model(x_natural), dim=1))
-            grad = torch.autograd.grad(loss_kl, [x_adv])[0]
-            for idx_batch in range(batch_size):
-                grad_idx = grad[idx_batch]
-                grad_idx_norm = l2_norm(grad_idx)
-                grad_idx /= (grad_idx_norm + 1e-8)
-                x_adv[idx_batch] = x_adv[idx_batch].detach() + step_size * grad_idx
-                eta_x_adv = x_adv[idx_batch] - x_natural[idx_batch]
-                norm_eta = l2_norm(eta_x_adv)
-                if norm_eta > epsilon:
-                    eta_x_adv = eta_x_adv * epsilon / l2_norm(eta_x_adv)
-                x_adv[idx_batch] = x_natural[idx_batch] + eta_x_adv
-            x_adv = torch.clamp(x_adv, 0.0, 1.0)
-    else:
+    for _ in range(perturb_steps):
+        x_adv.requires_grad_()
+        with torch.enable_grad():
+            loss_kl = criterion_kl(F.log_softmax(model(x_adv), dim=1),
+                                   F.softmax(model(x_natural), dim=1))
+        grad = torch.autograd.grad(loss_kl, [x_adv])[0]
+        x_adv = x_adv.detach() + step_size * torch.sign(grad.detach())
+        x_adv = torch.min(torch.max(x_adv, x_natural - epsilon), x_natural + epsilon)
         x_adv = torch.clamp(x_adv, 0.0, 1.0)
 
-    x_adv = Variable(torch.clamp(x_adv, 0.0, 1.0), requires_grad=False)
     # zero gradient
     optimizer.zero_grad()
 

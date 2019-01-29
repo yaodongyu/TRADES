8fa68a35104e8346505bfe8fa0da339ce689d283
Yaodong Yu <yaodong.yu92@gmail.com>
2019-01-28 19:54:14 -0500

    update trades loss

--- trades.py
+++ trades.py
@@ -13,27 +13,6 @@ def l2_norm(x):
     return squared_l2_norm(x).sqrt()
 
 
-def perturb_kl(model,
-               x_natural,
-               step_size=0.003,
-               epsilon=0.031,
-               perturb_steps=10):
-    # define KL-loss
-    criterion_kl = nn.KLDivLoss(size_average=False)
-    # init adversarial example
-    x_adv = x_natural.detach() + 0.001 * torch.randn(x_natural.shape).cuda().detach()
-    for _ in range(perturb_steps):
-        x_adv.requires_grad_()
-        with torch.enable_grad():
-            loss_kl = criterion_kl(F.log_softmax(model(x_adv), dim=1),
-                                   F.softmax(model(x_natural), dim=1))
-        grad = torch.autograd.grad(loss_kl, [x_adv])[0]
-        x_adv = x_adv.detach() + step_size * torch.sign(grad.detach())
-        x_adv = torch.min(torch.max(x_adv, x_natural - epsilon), x_natural + epsilon)
-        x_adv = torch.clamp(x_adv, 0.0, 1.0)
-    return x_adv
-
-
 def trades_loss(model,
                 x_natural,
                 y,

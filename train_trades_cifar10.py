10753d364819bcc54b7571916b981a7ff633481c
Hongyang Zhang <hongyanz@cs.cmu.edu>
2019-06-05 18:29:54 -0400

    Update train_trades_cifar10.py

--- train_trades_cifar10.py
+++ train_trades_cifar10.py
@@ -17,7 +17,7 @@ parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                     help='input batch size for training (default: 128)')
 parser.add_argument('--test-batch-size', type=int, default=128, metavar='N',
                     help='input batch size for testing (default: 128)')
-parser.add_argument('--epochs', type=int, default=76, metavar='N',
+parser.add_argument('--epochs', type=int, default=80, metavar='N',
                     help='number of epochs to train')
 parser.add_argument('--weight-decay', '--wd', default=2e-4,
                     type=float, metavar='W')

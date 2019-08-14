d83eded1381e76cb08a6f5b84945d7a3b2e84158
Yaodong Yu <37083238+yaodongyu@users.noreply.github.com>
2019-08-13 20:46:58 -0700

    Update mnist_example_trades.py
    update data transform

--- mnist_example_trades.py
+++ mnist_example_trades.py
@@ -113,13 +113,11 @@ def main():
         datasets.MNIST('../data', train=True, download=True,
                        transform=transforms.Compose([
                            transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
                        ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=False, transform=transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
         ])),
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
